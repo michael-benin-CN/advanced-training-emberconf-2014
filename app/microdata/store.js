e654d14108b8f50e54d18d46ce40234bc73c3c3b
Michael <mbenin.cn@gmail.com>
2014-03-24 18:28:32 -0400

    updated store

--- app/microdata/store.js
+++ app/microdata/store.js
@@ -1,49 +1,40 @@
-function buildRecord(type, data, store) {
-  var containerKey = 'model:' + type;
-
-  var factory = store.container.lookupFactory(containerKey);
-
-  var record = factory.create({
-    id: data.id,
-    $data: data
-  });
-
-  var id = data.id;
-
-  identityMapForType(type, store)[id] = record;
-
-  return record;
-}
-
-function identityMapForType(type, store) {
-  var typeIdentityMap = store.get('identityMap');
-
-  var idIdentityMap = typeIdentityMap[type] || {};
-  typeIdentityMap[type] = idIdentityMap;
-
-  return idIdentityMap;
-}
-
 export default Ember.Object.extend({
+
   init: function() {
     this.set('identityMap', {});
   },
 
+  buildRecord: function (type, data, store) {
+    var id = data.id;
+    var containerKey = 'model:' + type;
+    var factory = store.container.lookupFactory(containerKey);
+    var record = factory.create({
+      id: id,
+      $data: data
+    });
+    this.identityMapForType(type, store)[id] = record;
+    return record;
+  },
+
+  identityMapForType: function (type, store) {
+    var typeIdentityMap = store.get('identityMap');
+    var idIdentityMap = typeIdentityMap[type] || {};
+    typeIdentityMap[type] = idIdentityMap;
+    return idIdentityMap;
+  },
+
   push: function(type, data) {
     var record = this.getById(type, data.id);
-
     if (record) {
       record.set('$data', data);
     } else {
-      record = buildRecord(type, data, this);
+      record = this.buildRecord(type, data, this);
     }
-
     return record;
   },
 
   getById: function(type, id) {
-    var identityMap = identityMapForType(type, this);
-
+    var identityMap = this.identityMapForType(type, this);
     return identityMap[id] || null;
   }
 });
